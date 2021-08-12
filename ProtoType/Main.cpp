e26a4068d97b90574d1b67cedb18b7a24d07c29a
인석 장 <201630249@pukyong.ac.kr>
2021-08-13 05:18:59 +0900

    list로 변경

--- ProtoType/Main.cpp
+++ ProtoType/Main.cpp
@@ -1,71 +1,74 @@
 #include "PrototypeManager.h"
 #include "Object.h"
 
+//Object* pObjList[3];
 
-Object* pObjList[3];
+list<Object*> pObjList;
 
-void InitObject(string _Key)
-{
-	//** PrototypeManager 에서 원본 객체를 찾아옴.
-	Object* pObj = PrototypeManager::GetInstance()->FineObject(_Key);
+void InitObject(string _key) {
+	Object* pObj = PrototypeManager::GetInstance()->FineObject(_key);
 
-	//** 찾으려는 객체가 없다면...
-	if (pObj == nullptr)
-	{
-		//** 오류 메시지를 출력하고...
-		COORD Pos = { (SHORT)50, (SHORT)15 };
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+	if (!pObj) {
+		COORD pos = { (SHORT)50, (SHORT)15 };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 		cout << "복제할 원본 객체가 없습니다." << endl;
 
-		// ** 함수 종료.
 		return;
 	}
-
-
-	// ** 원형객체를 정상적으로 참조했다면...
-	// ** pObjList 를 초기화
-	for (int i = 0; i < 3; ++i)
+	/*
+	for (int i = 0; i < 3; i++)
 	{
-		// ** 참조된 원형 객체의 복사본을 넘겨줌
 		pObjList[i] = pObj->Clone();
-
-
-		// ** 그리고 본사본을 초기화함.
 		pObjList[i]->Initialize();
-
-		// ** 복사본의 인덱스값을 변경.
-		pObjList[i]->SetIndex(i + 1);
+		pObjList[i]->SetIndex(i+1);
+	}
+	*/
+	for (int i = 0; i < 3; i++)
+	{
+		pObjList.push_back(pObj->Clone());
+	}
+	int count = 0;
+	for (list<Object*>::iterator iter = pObjList.begin(); iter != pObjList.end();iter++)
+	{
+		(*iter)->Initialize();
+		(*iter)->SetIndex(++count);
 	}
 }
-
-
-
-int main(void)
-{
-	//** PrototypeManager 초기화 (초기화가 진행되면 원형객체를 생성한다.)
+int main() {
 	PrototypeManager::GetInstance()->Initialize();
 
-	//** 초기화
 	InitObject("Object");
-
-	//** 출력
-	for (int i = 0; i < 3; ++i)
-		if (pObjList[i])
-			pObjList[i]->Render(i);	
-
-	//** 삭제
-	for (int i = 0; i < 3; ++i)
+	/*
+	for (int i = 0; i < 3; i++)
 	{
-		if (pObjList[i])
-		{
+		if (pObjList[i])	pObjList[i]->Render(i);
+	}
+	*/
+	int count = 0;
+	for (list<Object*>::iterator iter = pObjList.begin(); iter != pObjList.end(); iter++)
+	{
+		if ((*iter))	(*iter)->Render(count++);
+	}
+	/*
+	for (int i = 0; i < 3; i++)
+	{
+		if (pObjList[i]) {
 			delete pObjList[i];
 			pObjList[i] = nullptr;
 		}
 	}
-	
+	*/
+	for (list<Object*>::iterator iter = pObjList.begin(); iter != pObjList.end(); iter++)
+	{
+		if ((*iter)) {
+			delete (*iter);
+			(*iter) = nullptr;
+		}
+	}
+	pObjList.clear();
+
 	return 0;
 }
-
 
 /**********************
  * 초기 구문
